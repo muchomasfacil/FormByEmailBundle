5d8206f773a70ec45fa0c1f244cfc13156bf0d89
alvaro_mmf <alvaro@muchomasfacil.com>
2013-05-06 15:36:18 +0200

    Container injection and first documentation

--- README.md
+++ README.md
@@ -1,7 +1,181 @@
-FormByEmailBundle
-=================
-
+# FormByEmailBundle
 Simplifies sending your Symfony2 forms by email
 
-TODO 
+## Install bundle with composer.json
+Add to your composer.json in the repositories (create it if not exist) section:
+
+``` json
+        {
+            "type": "vcs",
+            "url": "git@github.com:muchomasfacil/FormByEmailBundle.git"
+        }
+```
+
+You must add this entries to your app/AppKernel.php
+``` php
+            new MuchoMasFacil\FormByEmailBundle\MuchoMasFacilFormByEmailBundle(),
+```
+
+Enable translation in your app/config/config.yml (optional but recommended)
+``` yaml
+# ...
+framework:
+# ...
+    translator:      { fallback: %locale% }
+# ...    
+```
+
+Finally run on your project (take care of "minimum-stability" if necesary)
+``` bash
+composer.phar require muchomasfacil/InPageEditBundle dev-master
+```
+## Configure
+In your app/config/config.yml you can:
+``` yaml
+mucho_mas_facil_form_by_email:
+    definitions: 
+        # overwrite bundle default configuration
+        default:
+            template: MuchoMasFacilFormByEmailBundle:Default:formByEmail.html.twig
+            is_html: true        
+            recipients_setBcc: [{ info@mycompany.com: 'My company info'}, mypersonal@gmail.com: 'My personal mail'}]
+        # create your custom
+        my_txt_contact: 
+            template: MuchoMasFacilFormByEmailBundle:Default:formByEmail.txt.twig
+            is_html: false
+            recipients_addBcc: [ {mypersonal2@yahoo.es: 'Another recipient for the my_txt_contact form'} ]
+        
+```
+
+## Using FormByEmailBundle
+``` yaml
+// make sure you've imported the Request namespace above the class
+use Symfony\Component\HttpFoundation\Request;
+// ...
+
+public function contactAction(Request $request)
+{
+    $defaultData = array('message' => 'Type your message here');
+    $form = $this->createFormBuilder($defaultData)
+        ->add('name', 'text')
+        ->add('email', 'email')
+        ->add('message', 'textarea')
+        ->getForm();
+
+        if ($request->isMethod('POST')) {
+            $form->bind($request);
+
+            // data is an array with "name", "email", and "message" keys
+            $data = $form->getData();
+        }
+
+    // ... render the form
+}
+```
+
+## Parameters reference
+
+<table>
+    <tr>
+        <th>Param</th>
+        <th>Type</th>
+        <th>Default</th>
+        <th>Reference</th>
+    </tr>
+
+<tr>
+    <td>template</td>
+    <td>string<td>
+    <td>MuchoMasFacilFormByEmailBundle:Default:formByEmail.txt.twig</td>
+    <td></td>
+</tr>
+<tr>    
+    <td>is_html        
+    <td>boolean</td>
+    <td>false</td>
+    <td></td>
+</tr>
+<tr>    
+    <td>locale
+    <td>string</td>
+    <td>~</td>
+    <td></td>
+</tr>
+<tr>    
+    <td>translation_domain
+    <td>string</td>
+    <td>messages</td>
+    <td></td>
+</tr>
+<tr>    
+    <td>skip_fields
+    <td>array</td>
+    <td>[_token]</td>
+    <td></td>
+</tr>
+<tr>    
+    <td>sender_setFrom
+    <td>array</td>
+    <td>[]</td>
+    <td></td>
+</tr>
+<tr>    
+    <td>sender_setSender
+    <td>email address</td>
+    <td>~</td>
+    <td></td>
+</tr>
+<tr>    
+    <td>sender_setReturnPath
+    <td>email address</td>
+    <td>~</td>
+    <td></td>
+</tr>
+<tr>    
+    <td>sender_setReplyTo 
+    <td>email address</td>
+    <td>~</td>
+    <td></td>
+</tr>
+<tr>    
+    <td>recipients_setTo
+    <td>array</td>
+    <td>[]</td>
+    <td></td>
+</tr>
+<tr>    
+    <td>recipients_setCc
+    <td>array</td>
+    <td>[]</td>
+    <td></td>
+</tr>
+<tr>    
+    <td>recipients_setBcc
+    <td>array</td>
+    <td>[]</td>
+    <td></td>
+</tr>
+<tr>    
+    <td>recipients_addTo
+    <td>array</td>
+    <td>[]</td>
+    <td></td>
+</tr>
+<tr>    
+    <td>recipients_addCc
+    <td>array</td>
+    <td>[]</td>
+    <td></td>
+</tr>
+<tr>    
+    <td>recipients_addBcc
+    <td>array</td>
+    <td>[]</td>
+    <td></td>
+</tr>    
+</table>
+
+
+## TODO
 traducir mensajes de error (hacer un template flash...)
+DEFAULT FORM DATA??
--- Service/FormByEmail.php
+++ Service/FormByEmail.php
@@ -4,52 +4,33 @@ namespace MuchoMasFacil\FormByEmailBundle\Service;
 
 class FormByEmail
 {
-    private $templating;
+    private $container;
     
-    private $mailer;
+    private $definitions = array();
 
-    private $default_params = array(
-        'template' => 'MuchoMasFacilFormByEmailBundle:Default:formByEmail.txt.twig'
-        , 'is_html' => false        
-        , 'locale' => null
-        , 'translation_domain' => 'messages'
-        , 'skip_fields' => array('_token')
-        , 'sender_setFrom' => array()
-        , 'sender_setSender' => null
-        , 'sender_setReturnPath' => null
-        , 'sender_setReplyTo' => null
-        , 'recipients_setTo' => null
-        , 'recipients_setCc' => null
-        , 'recipients_setBcc' => null
-        , 'recipients_addTo' => array()
-        , 'recipients_addCc' => array()
-        , 'recipients_addBcc' => array()
-        
-    );
-
-    function __construct($definitions, $mailer, $templating) {
-        var_export($definitions);
-        die('hoola');
-        $this->templating = $templating;
-        $this->mailer = $mailer;
+    function __construct($container) {
+        $this->container = $container;
+        $this->definitions =  $this->container->getParameter('mucho_mas_facil_form_by_email.definitions');    
+        //var_export($this->definitions);
+        //die();
     }
     
     public function sendMail($body, $subject, $definition_to_load = null, $params = array())
     {
-        $result = true;
-
-        $params = $this->mergeParams($definition_to_load, $params);
-
+        $result = false;
+        $params = $this->mergeParams($definition_to_load, $params);        
         $message = \Swift_Message::newInstance()
             ->setFrom($params['sender_setFrom'])
             ->setSender($params['sender_setSender'])
             ->setReturnPath($params['sender_setReturnPath'])
-            ->setReplyTo($params['sender_setReplyTo'])
+            ->setReplyTo($params['sender_setReplyTo'])        
+            ->setSubject($subject)
+            ->setBody($body, ($params['is_html'])? 'text/html' : 'text/plain')
             ->setTo($params['recipients_setTo'])
             ->setCc($params['recipients_setCc'])
             ->setBcc($params['recipients_setBcc'])
-            ->setSubject($subject)
-            ->setBody($body, ($params['is_html'])? 'text/html' : 'text/plain');
+        ;
+
         foreach ($params['recipients_addTo'] as $entry) {
             $message->addTo(key($entry), current($entry));
         }        
@@ -61,7 +42,7 @@ class FormByEmail
         }
 
         try {
-            $sended_mails = $this->mailer->send($message);
+            $sended_mails = $this->container->get('mailer')->send($message);
             if ($sended_mails) {
                 $result = true;
                 $flash = array('type' => 'error', 'message' =>'success.email_sent');
@@ -83,26 +64,31 @@ class FormByEmail
         if (!is_null($params['locale'])) {
             $params['locale'] = $locale;
         }
-        $body = $this->templating->render($params['template'], array('subject' => $subject, 'form' => $form, 'params' => $params));
+        $body = $this->container->get('templating')->render($params['template'], array('subject' => $subject, 'form' => $form, 'params' => $params));
 
         return $this->sendMail($body, $subject, null, $params);
     }
 
-    public function formByEmail($request, $form, $subject, $definition_to_load = null, $params = array())
+    public function formByEmail($form, $empty_form_data, $subject, $definition_to_load = null, $params = array())
     {
         $result = false;
         $flash = null;
 
+        $request = $this->container->get('request');
         $params = $this->mergeParams($definition_to_load, $params);
         if (is_null($params['locale'])) {
             $params['locale'] = $request->getLocale();
         }        
 
+        $cloned_empty_form = clone $form;
+        $cloned_empty_form->setData($empty_form_data);
+
         if ($request->getMethod() == 'POST') {
             $form->bindRequest($request);
             if ($form->isValid()) {                
+                //this returns $result & $flash
                 extract($this->sendBindedformByEmail(null, $form, $subject, null, $params));      
-                //$form = ;                            
+                $form = $cloned_empty_form;
             }
             else {
                 $flash = array('type' => 'error', 'message' =>'error.form_invalid');
@@ -121,13 +107,15 @@ class FormByEmail
         if (isset($params['skip_merge_params'])) {
             return $params;
         }
-        $default_params = $this->default_params;
-        $definition_params = array();
-        if (!is_null($definition_to_load)) {
-            //load definition params
+        $default_params = $this->definitions['default'];
+        $definition_loaded_params = array();
+        if (isset($this->definitions[$definition_to_load])) {
+            $definition_loaded_params = $this->definitions[$definition_to_load];
         }
+                        
         $params['skip_merge_params'] = true; //this not to run mergeParams more than once
-        return array_merge($this->default_params, $definition_params, $params);
+
+        return array_merge($default_params, $definition_loaded_params, $params);
     }
     
 }
